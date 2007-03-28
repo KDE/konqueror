bd1723edc834664bf62d91beac0b109c84bbe577
Urs Wolfer <uwolfer@kde.org>
2007-03-28 20:15:42 +0000

    fix icons
    svn path=/trunk/KDE/kdebase/konqueror/; revision=647590

--- konqueror/konq_actions.cc
+++ konqueror/konq_actions.cc
@@ -260,7 +260,7 @@ QWidget * KonqLogoAction::createWidget( QWidget * parent )
   connect(this, SIGNAL(startAnimation()), button, SLOT(start()));
   connect(this, SIGNAL(stopAnimation()), button, SLOT(stop()));
 
-  button->setIcons(QString("kde"));
+  button->setIcons(QString("about-kde"));
 
   layout->addWidget(button);
 
