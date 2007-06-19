3add33518917c3d99d0c8215b75c1dff8f6facbb
David Faure <faure@kde.org>
2007-06-19 08:50:42 +0000

    Add missing file
    svn path=/trunk/KDE/kdebase/apps/; revision=677506

---
+++ konqueror/konq_framevisitor.h
@@ -0,0 +1,45 @@
+/*
+   This file is part of the KDE project
+   Copyright (C) 2007 David Faure <faure@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KONQ_FRAMEVISITOR_H
+#define KONQ_FRAMEVISITOR_H
+
+class KonqFrame;
+class KonqFrameContainer;
+class KonqFrameTabs;
+class KonqMainWindow;
+
+class KonqFrameVisitor
+{
+public:
+    KonqFrameVisitor() {}
+    virtual ~KonqFrameVisitor() {}
+    virtual bool visit(KonqFrame*) { return true; }
+    virtual bool visit(KonqFrameContainer*) { return true; }
+    virtual bool visit(KonqFrameTabs*) { return true; }
+    virtual bool visit(KonqMainWindow*) { return true; }
+
+    virtual bool endVisit(KonqFrameContainer*) { return true; }
+    virtual bool endVisit(KonqFrameTabs*) { return true; }
+    virtual bool endVisit(KonqMainWindow*) { return true; }
+};
+
+#endif /* KONQ_FRAMEVISITOR_H */
+
