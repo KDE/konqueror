f15fc9d4e5b969c6659e1da10de2e878270ade9b
David Faure <faure@kde.org>
2007-06-26 11:04:09 +0000

    cleanups
    svn path=/trunk/KDE/kdebase/apps/; revision=680487

--- konqueror/settings/konq/globalpaths.cpp
+++ konqueror/settings/konq/globalpaths.cpp
@@ -1,3 +1,24 @@
+/**
+ *  Copyright (c) Martin R. Jones 1996
+ *  Copyright (c) Bernd Wuebben 1998
+ *  Copyright (c) Christian Tibirna 1998
+ *  Copyright 1998-2007 David Faure <faure@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 //
 //
 // "Desktop Options" Tab for KDesktop configuration
@@ -23,11 +44,10 @@
 #include <QtGui/QLayout>
 #include <QtGui/QGridLayout>
 #include <QtGui/QDesktopWidget>
+#include <QtGui/QApplication>
 #include <QtDBus/QtDBus>
 
 // KDE
-#include <k3listview.h>
-#include <kapplication.h>
 #include <kconfiggroup.h>
 #include <kcustommenueditor.h>
 #include <kdebug.h>
@@ -40,7 +60,6 @@
 #include <klocale.h>
 #include <kmessagebox.h>
 #include <kmimetype.h>
-#include <konq_defaults.h> // include default values directly from libkonq
 #include <kstandarddirs.h>
 #include <kurlrequester.h>
 
--- konqueror/settings/konq/globalpaths.h
+++ konqueror/settings/konq/globalpaths.h
@@ -1,24 +1,38 @@
-//
+/**
+ *  Copyright (c) Martin R. Jones 1996
+ *  Copyright 1998-2007 David Faure <faure@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+// Summarized history:
 //
 // "Desktop Icons Options" Tab for KDesktop configuration
-//
-// (c) Martin R. Jones 1996
+// Martin Jones
 //
 // Port to KControl, split from "Misc" Tab, Port to KControl2
 // (c) David Faure 1998
 // Desktop menus, paths
 // (c) David Faure 2000
 
-#ifndef __ROOT_OPTIONS_H
-#define __ROOT_OPTIONS_H
-
-
-
+#ifndef __GLOBALPATHS_H
+#define __GLOBALPATHS_H
 
 #include <kcmodule.h>
 #include <kio/global.h>
 #include <kurl.h>
-
 
 class KJob;
 class KUrlRequester;
@@ -32,29 +46,30 @@ namespace KIO { class Job; }
 
 class DesktopPathConfig : public KCModule
 {
-        Q_OBJECT
+    Q_OBJECT
 public:
-        DesktopPathConfig( QWidget *parent, const QStringList &args );
-        virtual void load();
-        virtual void save();
-        virtual void defaults();
+    DesktopPathConfig( QWidget *parent, const QStringList &args );
+    virtual void load();
+    virtual void save();
+    virtual void defaults();
 
 private Q_SLOTS:
     void slotEntries( KIO::Job * job, const KIO::UDSEntryList& list);
 
 private:
-        // Desktop Paths
-        KUrlRequester *urDesktop;
-        KUrlRequester *urAutostart;
-        KUrlRequester *urDocument;
+    // Desktop Paths
+    KUrlRequester *urDesktop;
+    KUrlRequester *urAutostart;
+    KUrlRequester *urDocument;
 
-        bool moveDir( const KUrl & src, const KUrl & dest, const QString & type );
-        bool m_ok;
-        KUrl m_copyToDest; // used when the destination directory already exists
-        KUrl m_copyFromSrc;
+    bool moveDir( const KUrl & src, const KUrl & dest, const QString & type );
+    bool m_ok;
+    KUrl m_copyToDest; // used when the destination directory already exists
+    KUrl m_copyFromSrc;
 
 private Q_SLOTS:
-        void slotResult( KJob * job );
+    void slotResult( KJob * job );
 };
 
-#endif // __ROOT_OPTIONS_H
+#endif
+
