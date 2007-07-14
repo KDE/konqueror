9d802a2d8da26e4884c52f3a405821e4245076c6
Dirk Mueller <mueller@kde.org>
2007-07-14 21:07:22 +0000

    move configure checks for kfontinst
    svn path=/trunk/KDE/kdebase/apps/; revision=687954

---
+++ ConfigureChecks.cmake
@@ -0,0 +1,11 @@
+include(CheckIncludeFile)
+include(CheckIncludeFiles)
+include(CheckSymbolExists)
+include(CheckFunctionExists)
+include(CheckLibraryExists)
+include(CheckPrototypeExists)
+include(CheckStructMember)
+include(CheckTypeSize)
+include(CheckCXXSourceCompiles)
+
+check_function_exists(getpeereid  HAVE_GETPEEREID) # kdesu
