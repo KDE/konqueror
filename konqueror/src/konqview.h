a183618368ba57536eb9d87c6719af03e3db50a7
Germain Garand <germain@ebooksfrance.org>
2007-07-23 00:59:36 +0000

    reinstating patch by Hasso Tepper that was once reverted for BIC problems, then forgotten.
    (kdebase part)
    
    BUG: 66958
    
    
    svn path=/trunk/KDE/kdebase/apps/; revision=691145

--- konqueror/src/konqview.h
+++ konqueror/src/konqview.h
@@ -319,7 +319,7 @@ public:
 
   // Called before reloading this view. Sets args.reload to true, and offers to repost form data.
   // Returns false in case the reload must be canceled.
-  bool prepareReload( KParts::URLArgs& args );
+  bool prepareReload( KParts::URLArgs& args, bool softReload );
 
   // overload for the QString version
   void setLocationBarURL( const KUrl& locationBarURL );
