282fea26ef2da3af9ec40fc4d45f2e1f0badcc31
David Faure <faure@kde.org>
2007-11-22 23:55:34 +0000

    RELICENSE: GPLv2+ -> LGPLv2-or-v3 as agreed with non-trivial contributors (now listed explicitely in the copyright line of both files)
    svn path=/trunk/KDE/kdebase/apps/; revision=740314

--- lib/konq/konq_operations.cpp
+++ lib/konq/konq_operations.cpp
@@ -1,10 +1,14 @@
 /*  This file is part of the KDE project
-    Copyright (C) 2000  David Faure <faure@kde.org>
+    Copyright 2000-2007  David Faure <faure@kde.org>
+    Copyright 2003       Waldo Bastian <bastian@kde.org>
+    Copyright 2002       Michael Brade <brade@kde.org>
+    Copyright 2001-2002  Alexander Neundorf <neundorf@kde.org>
+    Copyright 2000-2001  Simon Hausmann <hausmann@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) version 3.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
