47c34fce6ba543de97c5a804313053b10c77b22b
Dirk Mueller <mueller@kde.org>
2007-11-28 22:50:43 +0000

    RELICENSE: GPLv2 -> GPLv2+
    svn path=/trunk/KDE/kdebase/apps/; revision=742797

--- konqueror/settings/filetypes/filegroupdetails.h
+++ konqueror/settings/filetypes/filegroupdetails.h
@@ -1,9 +1,10 @@
 /* This file is part of the KDE project
    Copyright (C) 2000 David Faure <faure@kde.org>
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License version 2 as published by the Free Software Foundation.
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
