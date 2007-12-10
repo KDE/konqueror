a40f6fb938f4f9c2ef0aa9d7f32938604c3e2599
Adriaan de Groot <groot@kde.org>
2007-12-10 16:24:33 +0000

    SS12 uses __i386 (and __amd64, but haven't hit that case yet)
    svn path=/trunk/KDE/kdebase/apps/; revision=746920

--- nsplugins/sdk/prcpucfg.h
+++ nsplugins/sdk/prcpucfg.h
@@ -321,7 +321,7 @@
 #define PR_BYTES_PER_WORD_LOG2   2
 #define PR_BYTES_PER_DWORD_LOG2  3
 
-#elif defined(__i386__)
+#elif defined(__i386__) || (defined(__SUNPRO_CC) && defined(__i386))
 
 #define IS_LITTLE_ENDIAN 1
 #undef  IS_BIG_ENDIAN
