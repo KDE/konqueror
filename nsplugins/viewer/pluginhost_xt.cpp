3f9df95c98011cc748a49f3b2a0d89fdaf2dd05e
Maks Orlovich <maksim@kde.org>
2007-12-13 20:44:08 +0000

    Refactor some duplicated stuff out...
    svn path=/trunk/KDE/kdebase/apps/; revision=748198

--- nsplugins/viewer/pluginhost_xt.cpp
+++ nsplugins/viewer/pluginhost_xt.cpp
@@ -139,32 +139,7 @@ void PluginHostXt::setupWindow(int width, int height)
     // Embed the Xt widget into the Qt widget
     XReparentWindow(QX11Info::display(), XtWindow(_toplevel), _outside->winId(), 0, 0);
     XtMapWidget(_toplevel);
-    NPWindow win;
-    win.x = 0;
-    win.y = 0;
-    win.width  = width;
-    win.height = height;
-    win.type = NPWindowTypeWindow;
-    
-    // Well, the docu says sometimes, this is only used on the
-    // MAC, but sometimes it says it's always. Who knows...
-    win.clipRect.top  = 0;
-    win.clipRect.left = 0;
-    win.clipRect.bottom = height;
-    win.clipRect.right  = width;
-    
-    win.window =  (void*) XtWindow(_form);
-    QX11Info x11info;
-    NPSetWindowCallbackStruct win_info;
-    win_info.display  = QX11Info::display();
-    win_info.visual   = (Visual*) x11info.visual();
-    win_info.colormap = x11info.colormap();
-    win_info.depth    = x11info.depth();
-    win.ws_info = &win_info;
-    
-    NPError error = _plugin->NPSetWindow( &win );
-    
-    kDebug(1431) << "error = " << error;
+    setupPluginWindow(_plugin, (void*) XtWindow(_form), width, height);
 }
 
 // kate: indent-width 4; replace-tabs on; tab-width 4; space-indent on;
