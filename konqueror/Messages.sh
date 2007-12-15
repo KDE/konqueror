73535775f41b5271b2f22a077f2f38478ccbf9e9
Chusslove Illich <caslav.ilic@gmx.net>
2007-12-15 13:26:23 +0000

    Do not forget rc.cpp
    svn path=/trunk/KDE/kdebase/apps/; revision=748774

--- konqueror/Messages.sh
+++ konqueror/Messages.sh
@@ -2,4 +2,4 @@
 subdirs="src sidebar about kttsplugin remoteencodingplugin shellcmdplugin"
 $EXTRACTRC `find $subdirs -name \*.rc` >> rc.cpp || exit 11
 $EXTRACTRC `find $subdirs -name \*.ui` >> rc.cpp || exit 12
-$XGETTEXT -kaliasLocal `find $subdirs -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/konqueror.pot
+$XGETTEXT -kaliasLocal `find $subdirs -name \*.cc -o -name \*.cpp -o -name \*.h` rc.cpp -o $podir/konqueror.pot
