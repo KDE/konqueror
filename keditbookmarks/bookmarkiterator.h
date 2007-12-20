815aac10fd39a6d11e15000c84706a28380c5876
Tom Albers <toma@kde.org>
2007-12-20 22:23:07 +0000

    RELICENSE: GPL2->GPL2+/GPL23
    svn path=/trunk/KDE/kdebase/apps/; revision=751022

--- keditbookmarks/bookmarkiterator.h
+++ keditbookmarks/bookmarkiterator.h
@@ -3,9 +3,9 @@
    Copyright (C) 2002-2003 Alexander Kellett <lypanov@kde.org>
 
    This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) version 3.
+   modify it under the terms of the GNU General Public
+   License version 2 or at your option version 3 as published by
+   the Free Software Foundation.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
--- keditbookmarks/importers.cpp
+++ keditbookmarks/importers.cpp
@@ -6,12 +6,13 @@
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
-   License version 2 as published by the Free Software Foundation.
+   License version 2 or at your option version 3 as published by
+   the Free Software Foundation.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
+   General Public License for more details.
 
    You should have received a copy of the GNU General Public License
    along with this program; see the file COPYING.  If not, write to
