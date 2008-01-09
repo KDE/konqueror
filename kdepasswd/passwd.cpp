2ef7af9e593d43d87c201a21bbe6d19476991ce8
Dirk Mueller <mueller@kde.org>
2008-01-09 13:13:05 +0000

    RELICENSE: Artistic License -> MIT
    svn path=/trunk/KDE/kdebase/apps/; revision=758884

--- kdepasswd/passwd.cpp
+++ kdepasswd/passwd.cpp
@@ -2,7 +2,25 @@
  *
  * This file is part of the KDE project, module kdesu.
  * Copyright (C) 1999,2000 Geert Jansen <jansen@kde.org>
- *
+
+ Permission to use, copy, modify, and distribute this software
+ and its documentation for any purpose and without fee is hereby
+ granted, provided that the above copyright notice appear in all
+ copies and that both that the copyright notice and this
+ permission notice and warranty disclaimer appear in supporting
+ documentation, and that the name of the author not be used in
+ advertising or publicity pertaining to distribution of the
+ software without specific, written prior permission.
+
+ The author disclaim all warranties with regard to this
+ software, including all implied warranties of merchantability
+ and fitness.  In no event shall the author be liable for any
+ special, indirect or consequential damages or any damages
+ whatsoever resulting from loss of use, data or profits, whether
+ in an action of contract, negligence or other tortious action,
+ arising out of or in connection with the use or performance of
+ this software.
+
  * passwd.cpp: Change a user's password.
  */
 
