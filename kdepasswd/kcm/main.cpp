f6e21c289e64e4c722a7a1b6ba1dceb545d5c818
Laurent Montel <montel@kde.org>
2008-02-02 13:08:55 +0000

    Disable help button when we don't have doc
    svn path=/trunk/KDE/kdebase/apps/; revision=769936

--- kdepasswd/kcm/main.cpp
+++ kdepasswd/kcm/main.cpp
@@ -102,6 +102,7 @@ KCMUserAccount::KCMUserAccount( QWidget *parent, const QVariantList &)
 			"will be used in mail programs and word processors, for example. You can "
 			"change your login password by clicking <em>Change Password</em>.</qt>") );
 
+        setButtons( KCModule::Default|KCModule::Apply );
 	load();
 }
 
