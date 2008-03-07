665decb96914d3ff7d9c7557b47b835281ae846a
Claudio Bantaloukas <rockdreamer@gmail.com>
2008-03-07 14:20:44 +0000

    fix EBN errors: header rearrangements, guards
    svn path=/trunk/KDE/kdebase/apps/; revision=783227

--- nsplugins/test/testnsplugin.cpp
+++ nsplugins/test/testnsplugin.cpp
@@ -18,8 +18,11 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
-#include <stdio.h>
 
+#include "testnsplugin.h"
+#include "../nspluginloader.h"
+
+#include <stdio.h>
 
 #include <kactioncollection.h>
 //Added by qt3to4:
@@ -29,8 +32,6 @@
 #include <kstandardaction.h>
 #include <kaction.h>
 
-#include "testnsplugin.h"
-#include "../nspluginloader.h"
 
 
 TestNSPlugin::TestNSPlugin()
