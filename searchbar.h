7790b7f27e600ea561ba9e44c72e9cfca5ab2ae9
Laurent Montel <montel@kde.org>
2008-06-15 12:03:25 +0000

    Fix comment

--- searchbar.h
+++ searchbar.h
@@ -19,7 +19,7 @@
  * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
- *
+ */
 
 #ifndef SEARCHBAR_H
 #define SEARCHBAR_H
