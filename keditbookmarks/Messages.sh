d8f595d77120edc0f26d925bd414afd9ada8111c
Laurent Montel <montel@kde.org>
2008-07-02 07:04:51 +0000

    Remove temporary file
    svn path=/trunk/KDE/kdebase/apps/; revision=827042

--- keditbookmarks/Messages.sh
+++ keditbookmarks/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
 $XGETTEXT -kaliasLocal `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/keditbookmarks.pot
+rm -f rc.cpp
--- nsplugins/Messages.sh
+++ nsplugins/Messages.sh
@@ -2,3 +2,4 @@
 #	$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
 $EXTRACTRC `find . -name "*.rc"` >> rc.cpp || exit 12
 $XGETTEXT *.cpp viewer/*.cpp -o $podir/nsplugin.pot
+rm -f rc.cpp
