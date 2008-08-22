4f5fe246a1c749b6fe6612b248e1a3b7a0df5443
Urs Wolfer <uwolfer@kde.org>
2008-08-22 20:56:55 +0000

    extract messages again after move

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp || exit 11
-$EXTRACTRC *.rc >> rc.cpp || exit 12
-$XGETTEXT *.cpp -o $podir/webkitkde.pot
+$EXTRACTRC *.ui */*.ui >> rc.cpp || exit 11
+$EXTRACTRC *.rc */*.rc >> rc.cpp || exit 12
+$XGETTEXT *.cpp */*.cpp -o $podir/webkitkde.pot
 rm -f rc.cpp
