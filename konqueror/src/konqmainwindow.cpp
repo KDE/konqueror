268311ac7f1b27a21d5507ed90a2e34974b23bc8
David Faure <faure@kde.org>
2008-09-12 23:24:08 +0000

    backport r860411, obvious fix
    svn path=/branches/KDE/4.1/kdebase/apps/; revision=860412

--- konqueror/src/konqmainwindow.cpp
+++ konqueror/src/konqmainwindow.cpp
@@ -1832,7 +1832,7 @@ void KonqMainWindow::slotConfigureDone()
 void KonqMainWindow::slotConfigureSpellChecking()
 {
     Sonnet::ConfigDialog dialog( KGlobal::config().data(), this);
-    setWindowIcon( KIcon( "konqueror" ));
+    dialog.setWindowIcon( KIcon( "konqueror" ));
     dialog.exec();
 }
 
