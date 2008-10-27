5d299ee2b7b1e0c4e87c59feea0d0e1b41d8e43a
Sebastian Sauer <mail@dipe.org>
2008-10-27 15:16:23 +0000

    * comments++ * copyright++ * naming++
    svn path=/trunk/KDE/kdebase/apps/; revision=876584

--- nsplugins/viewer/nsplugin.h
+++ nsplugins/viewer/nsplugin.h
@@ -1,10 +1,13 @@
 /*
-
   This is an encapsulation of the  Netscape plugin API.
 
   Copyright (c) 2000 Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
                      Stefan Schimanski <1Stein@gmx.de>
-  Copyright (c) 2003-2005 George Staikos <staikos@kde.org>
+                2003-2005 George Staikos <staikos@kde.org>
+                2007, 2008 Maksim Orlovich     <maksim@kde.org>
+                2006, 2007, 2008 Apple Inc.
+                2008 Collabora, Ltd.
+                2008 Sebastian Sauer <mail@dipe.org>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -19,9 +22,7 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 */
-
 
 #ifndef __NS_PLUGIN_H__
 #define __NS_PLUGIN_H__
@@ -176,9 +177,9 @@ public:
   void gotFocusIn();
   void gotFocusOut();
 
-  // sets and returns the current/last used NSPluginInstance instance.
-  static NSPluginInstance* currentPluginView();
-  static void setCurrentPluginView(NSPluginInstance*);
+  // last via NSPluginClass::newInstance() produced NSPluginInstance instance.
+  static NSPluginInstance* lastPluginInstance();
+  static void setLastPluginInstance(NSPluginInstance*);
 
   // value handling
   NPError NPGetValue(NPPVariable variable, void *value);
@@ -273,7 +274,7 @@ private:
   QQueue<Request *> _waitingRequests;
   QMap<int, Request*> _jsrequests;
   
-  static NSPluginInstance* s_currentPluginView;
+  static NSPluginInstance* s_lastPluginInstance;
 };
 
 
