2cc7d91c5d355c4cc9d8a39ec40c1afebf8a0ff3
Albert Astals Cid <tsdgeos@terra.es>
2008-10-30 19:10:01 +0000

    Lowercase the surname
    svn path=/trunk/KDE/kdebase/apps/; revision=877952

--- konqueror/sidebar/konqsidebar.cpp
+++ konqueror/sidebar/konqsidebar.cpp
@@ -114,7 +114,7 @@ const KComponentData &KonqSidebarFactory::componentData()
 	if( !s_instance )
 	{
 		s_about = new KAboutData("konqsidebartng", 0, ki18n("Extended Sidebar"), "0.1");
-		s_about->addAuthor(ki18n("Joseph WENNINGER"), KLocalizedString(), "jowenn@bigfoot.com");
+		s_about->addAuthor(ki18n("Joseph Wenninger"), KLocalizedString(), "jowenn@bigfoot.com");
 		s_instance = new KComponentData(s_about);
 	}
 	return *s_instance;
