11c23a2fd1ad7f23abbd3530ed080a891e0ce8f3
Thomas Zander <zander@kde.org>
2008-10-30 08:56:46 +0000

    Get rid of silly compiler warnings

--- part/webkitpart.cpp
+++ part/webkitpart.cpp
@@ -165,10 +165,13 @@ void WebKitPart::loadFinished()
 void WebKitPart::urlChanged(const QUrl &url)
 {
     const QList<QWebHistoryItem> backItemsList = view()->history()->backItems(2);
-    if (backItemsList.count() > 0)
+#ifndef NDEBUG
+    if (backItemsList.count() > 0) {
         kDebug() << backItemsList.at(0).url() << url;
+    }
+#endif
 
-    if (!((backItemsList.count() > 0) && (backItemsList.at(0).url() == url))) {
+    if (!(backItemsList.count() > 0 && backItemsList.at(0).url() == url)) {
         emit m_browserExtension->openUrlNotify();
     }
 
