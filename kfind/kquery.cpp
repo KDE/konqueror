f790e14d6cda066c0470f00985ef90535c11a980
Darío Andrés Rodríguez <andresbajotierra@gmail.com>
2009-03-08 22:32:41 +0000

    Better way to do svn commit 937050
    svn path=/trunk/KDE/kdebase/apps/; revision=937053

--- kfind/kquery.cpp
+++ kfind/kquery.cpp
@@ -336,9 +336,7 @@ void KQuery::processQuery( const KFileItem &file)
            kWarning() << "Cannot open supposed ZIP file " << file.url() ;
          }
        } else if( !m_search_binary && !file.mimetype().startsWith("text/") &&
-           file.url().isLocalFile() ) {
-           
-         if ( !file.url().path().startsWith("/dev") ) //Skip /dev files (isBinaryData reads from it)
+           file.url().isLocalFile() && !file.url().path().startsWith("/dev") ) {
          if ( KMimeType::isBinaryData(file.url().path()) ) {
            kDebug() << "ignoring, not a text file: " << file.url();
            return;
