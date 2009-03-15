19d42fc796da429f4546246be6b39c2aee3bc0c6
Pino Toscano <pino@kde.org>
2009-03-15 12:15:24 +0000

    fix message extraction

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC */*.ui >> rc.cpp || exit 11
-$EXTRACTRC */*.rc >> rc.cpp || exit 12
-$XGETTEXT */*.cpp -o $podir/webkitkde.pot
+$EXTRACTRC `find . -name '*.rc' -or -name '*.ui'` >> rc.cpp || exit 11
+$XGETTEXT `find . -name '*.cpp'` -o $podir/webkitkde.pot
 rm -f rc.cpp
