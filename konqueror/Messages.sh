0dfbcda602629eb72ef47eb8e02eecfd25f55281
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:09:27 +0000

    Updated konqueror/Messages.sh to pick up the konqueror.kcfg message strings too.
    svn path=/trunk/KDE/kdebase/apps/; revision=951669

--- konqueror/Messages.sh
+++ konqueror/Messages.sh
@@ -2,5 +2,6 @@
 subdirs="src sidebar about shellcmdplugin"
 $EXTRACTRC `find $subdirs -name \*.rc` >> rc.cpp || exit 11
 $EXTRACTRC `find $subdirs -name \*.ui` >> rc.cpp || exit 12
+$EXTRACTRC `find $subdirs -name \*.kcfg` >> rc.cpp
 $XGETTEXT -kaliasLocal `find $subdirs -name \*.cc -o -name \*.cpp -o -name \*.h` rc.cpp -o $podir/konqueror.pot
 rm -f rc.cpp
