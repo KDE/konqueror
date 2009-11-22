184cc8b34b50f22a1df93e41ec518dacae6a6f20
Alexander Neundorf <neundorf@kde.org>
2009-11-22 14:54:44 +0000

    -remove the last remnants from lanbrowsing from svn (kcm is in tags/unmaintained/)
    Alex
    
    svn path=/trunk/KDE/kdebase/apps/; revision=1052816

--- konqueror/settings/kio/main.cpp
+++ konqueror/settings/kio/main.cpp
@@ -1,9 +1,9 @@
 // (c) Torben Weis 1998
 // (c) David Faure 1998
 /*
- * main.cpp for lisa,reslisa,kio_lan and kio_rlan kcm module
+ * main.cpp for creating the konqueror kio kcm modules
  *
- *  Copyright (C) 2000,2001 Alexander Neundorf <neundorf@kde.org>
+ *  Copyright (C) 2000,2001,2009 Alexander Neundorf <neundorf@kde.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -19,9 +19,6 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
-// Own
-#include "main.h"
 
 // Qt
 #include <QtCore/QFile>
@@ -45,7 +42,6 @@
 #include "bookmarks.h"
 
 K_PLUGIN_FACTORY(KioConfigFactory,
-        registerPlugin<LanBrowser>("lanbrowser");
         registerPlugin<UserAgentDlg>("useragent");
         registerPlugin<SMBRoOptions>("smb");
         registerPlugin<KIOPreferences>("netpref");
@@ -55,83 +51,4 @@ K_PLUGIN_FACTORY(KioConfigFactory,
         registerPlugin<BookmarksConfigModule>("bookmarks");
 	)
 K_EXPORT_PLUGIN(KioConfigFactory("kcmkio"))
-
-LanBrowser::LanBrowser(QWidget *parent, const QVariantList &)
-    : KCModule(KioConfigFactory::componentData(), parent)
-    , layout(this)
-    , tabs(this)
-{
-   setQuickHelp( i18n("<h1>Local Network Browsing</h1>Here you setup your "
-		"<b>\"Network Neighborhood\"</b>. You "
-		"can use either the LISa daemon and the lan:/ ioslave, or the "
-		"ResLISa daemon and the rlan:/ ioslave.<br /><br />"
-		"About the <b>LAN ioslave</b> configuration:<br /> If you select it, the "
-		"ioslave, <i>if available</i>, will check whether the host "
-		"supports this service when you open this host. Please note "
-		"that paranoid people might consider even this to be an attack.<br />"
-		"<i>Always</i> means that you will always see the links for the "
-		"services, regardless of whether they are actually offered by the host. "
-		"<i>Never</i> means that you will never have the links to the services. "
-		"In both cases you will not contact the host, so nobody will ever regard "
-		"you as an attacker.<br /><br />More information about <b>LISa</b> "
-		"can be found at <a href=\"http://lisa-home.sourceforge.net\">"
-		"the LISa Homepage</a> or contact Alexander Neundorf "
-		"&lt;<a href=\"mailto:neundorf@kde.org\">neundorf@kde.org</a>&gt;."));
-   
-   layout.setMargin(0);
-   layout.addWidget(&tabs);
-
-   smbPage = new SMBRoOptions(&tabs, QVariantList(), componentData());
-   tabs.addTab(smbPage, i18n("&Windows Shares"));
-   connect(smbPage,SIGNAL(changed(bool)), SIGNAL( changed(bool) ));
-
-   lisaPage = KCModuleLoader::loadModule("kcmlisa", KCModuleLoader::None,&tabs);
-   if (lisaPage)
-   {
-     tabs.addTab(lisaPage,i18n("&LISa Daemon"));
-     connect(lisaPage,SIGNAL(changed()), SLOT( changed() ));
-   }
-
-//   resLisaPage = KCModuleLoader::loadModule("kcmreslisa", KCModuleLoader::None,&tabs);
-//   if (resLisaPage)
-//   {
-//     tabs.addTab(resLisaPage,i18n("R&esLISa Daemon"));
-//     connect(resLisaPage,SIGNAL(changed()), SLOT( changed() ));
-//   }
-
-   kioLanPage = KCModuleLoader::loadModule("kcmkiolan", KCModuleLoader::None, &tabs);
-   if (kioLanPage)
-   {
-     tabs.addTab(kioLanPage,i18n("lan:/ Iosla&ve"));
-     connect(kioLanPage,SIGNAL(changed()), SLOT( changed() ));
-   }
-
-   setButtons(Apply|Help);
-}
-
-void LanBrowser::load()
-{
-   smbPage->load();
-   if (lisaPage)
-     lisaPage->load();
-//   if (resLisaPage)
-//     resLisaPage->load();
-   if (kioLanPage)
-     kioLanPage->load();
-   emit changed(false);
-}
-
-void LanBrowser::save()
-{
-   smbPage->save();
-//   if (resLisaPage)
-//     resLisaPage->save();
-   if (kioLanPage)
-     kioLanPage->save();
-   if (lisaPage)
-     lisaPage->save();
-   emit changed(false);
-}
-
-#include "main.moc"
 
