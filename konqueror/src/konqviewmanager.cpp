1cd47e22935264edf95b5178396ddd518dfb2164
David Faure <faure@kde.org>
2009-12-01 15:43:26 +0000

    Found it! Fix the "statusbar rendering bug when restoring multiple tabs" bug. It was "simply" a wrong show() on a widget that is inside a tabwidget; we ended up with multiple widgets in the stackedwidget shown at the same time, bad idea... Fixed for: 4.4.0-beta2 BUG: 169124
    svn path=/trunk/KDE/kdebase/apps/; revision=1057148

--- konqueror/src/konqviewmanager.cpp
+++ konqueror/src/konqviewmanager.cpp
@@ -362,6 +362,10 @@ KonqMainWindow *KonqViewManager::openSavedWindow(const KConfigGroup& configGroup
     mainWindow->viewManager()->loadRootItem( configGroup, mainWindow, KUrl(), true, KUrl() );
     mainWindow->applyMainWindowSettings( configGroup, true );
     mainWindow->activateChild();
+
+#ifdef DEBUG_VIEWMGR
+    mainWindow->viewManager()->printFullHierarchy();
+#endif
     return mainWindow;
 }
 
@@ -373,6 +377,9 @@ KonqMainWindow *KonqViewManager::openSavedWindow(const KConfigGroup& configGroup
         return KonqViewManager::openSavedWindow(configGroup);
     } else {
         loadRootItem( configGroup, tabContainer(), KUrl(), true, KUrl() );
+#ifndef NDEBUG
+        printFullHierarchy();
+#endif
         return m_pMainWindow;
     }
 }
@@ -827,7 +834,9 @@ KonqView *KonqViewManager::setupView( KonqFrameContainerBase *parentContainer,
 
   parentContainer->insertChildFrame( newViewFrame, index );
 
-  if (parentContainer->frameType() != KonqFrameBase::Tabs) newViewFrame->show();
+  if (parentContainer->frameType() != KonqFrameBase::Tabs) {
+      newViewFrame->show();
+  }
 
   // Don't register passive views to the part manager
   if ( !v->isPassiveMode() ) // note that KonqView's constructor could set this to true even if passiveMode is false
@@ -1154,7 +1163,9 @@ void KonqViewManager::loadItem( const KConfigGroup &cfg, KonqFrameContainerBase 
         prefix = name + QLatin1Char( '_' );
     }
 
-    //kDebug() << "begin name=" << name << "openUrl=" << openUrl;
+#ifdef DEBUG_VIEWMGR
+    kDebug() << "begin name=" << name << "openUrl=" << openUrl;
+#endif
 
     if (name.startsWith("View") || name == "empty") {
         // load view config
@@ -1210,8 +1221,6 @@ void KonqViewManager::loadItem( const KConfigGroup &cfg, KonqFrameContainerBase 
         KonqConfigEvent ev( cfg.config(), prefix+'_', false/*load*/);
         QApplication::sendEvent( childView->part(), &ev );
 #endif
-
-        childView->frame()->show();
 
         if (parent == m_tabContainer && m_tabContainer->count() == 1) {
             // First tab, make it the active one
@@ -1515,7 +1524,7 @@ public:
             className = frame->part()->widget()->metaObject()->className();
         kDebug() << m_spaces << frame
                      << "parent=" << frame->parentContainer()
-                     << (frame->isVisible() ? "visible" : "invisible")
+                     << (frame->isHidden() ? "hidden" : "shown")
                      << "containing view" << frame->childView()
                      << "and part" << frame->part()
                      << "whose widget is a" << className;
@@ -1523,7 +1532,7 @@ public:
     }
     virtual bool visit(KonqFrameContainer* container) {
         kDebug() << m_spaces << container
-                     << (container->isVisible() ? "visible" : "invisible")
+                     << (container->isHidden() ? "hidden" : "shown")
                      << (container->orientation() == Qt::Horizontal ? "horizontal" : "vertical")
                      << "sizes=" << container->sizes()
                      << "parent=" << container->parentContainer()
@@ -1585,7 +1594,9 @@ bool KonqViewManager::isTabBarVisible() const
 
 void KonqViewManager::createTabContainer(QWidget* parent, KonqFrameContainerBase* parentContainer)
 {
-    //kDebug() << "createTabContainer" << parent << parentContainer;
+#ifdef DEBUG_VIEWMGR
+    kDebug() << "createTabContainer" << parent << parentContainer;
+#endif
     m_tabContainer = new KonqFrameTabs( parent, parentContainer, this );
     connect( m_tabContainer, SIGNAL(ctrlTabPressed()), m_pMainWindow, SLOT(slotCtrlTabPressed()) );
     // Delay the opening of the URL for #106641
