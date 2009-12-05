4e9cf81c227381e3e00e7d1288e63c89ffc675de
Dawit Alemayehu <adawit@kde.org>
2009-12-05 09:24:06 +0000

    More fixes to exclude more pages where we should not be saving form information

--- kdewebkit/resources/parseForms.js
+++ kdewebkit/resources/parseForms.js
@@ -7,9 +7,11 @@
             formObject.name = form.name;
             formObject.index = i.toString();
             var elements = new Array;
+            var passwordFieldCount = 0;
             for (var j = 0; j < form.elements.length; ++j) {
                 var e = form.elements[j];
-                if ((e.type.toLowerCase() == "password" || e.type.toLowerCase() == "text") && e.value.length > 0) {
+                var type = e.type.toLowerCase();
+                if ((type == "password" || type == "text") && e.value.length > 0) {
                     var element = new Object;
                     element.name = e.name;
                     element.value = e.value;
@@ -18,10 +20,12 @@
                     if (element.autocomplete != null)
                         element.autocomplete = element.autocomplete.value;
                     elements.push(element);
+                    if (type == "password")
+                      passwordFieldCount++;
                 }
             }
 
-            if (elements.length > 0) {
+            if (elements.length > 0 && passwordFieldCount > 0) {
                 formObject.elements = elements;
                 forms.push(formObject);
             }
