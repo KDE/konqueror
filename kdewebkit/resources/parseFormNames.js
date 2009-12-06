44ffc071343ad9ba9e6bf53001b9fa96e3d9572c
Dawit Alemayehu <adawit@kde.org>
2009-12-06 03:15:42 +0000

    Use toLowerCase() for the type comparison.

--- kdewebkit/resources/parseFormNames.js
+++ kdewebkit/resources/parseFormNames.js
@@ -2,7 +2,7 @@
     var forms = new Array;
     for (var i = 0; i < document.forms.length; ++i) {
         var form = document.forms[i];
-        if (form.method == "post") {
+        if (form.method.toLowerCase() == "post") {
             var formObject = new Object;
             formObject.name = form.name;
             formObject.index = i.toString();
