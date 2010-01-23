7d9383678ee0bf76d6b23bebde457d2e6ede0b80
Dawit Alemayehu <adawit@kde.org>
2010-01-23 23:12:51 +0000

    Fixed the library and header file names...

--- part/cmake/modules/FindWebKitPart.cmake
+++ part/cmake/modules/FindWebKitPart.cmake
@@ -5,10 +5,9 @@
 #  WEBKITPART_INCLUDE_DIR - the WebKitPart include directory
 #  WEBKITPART_LIBRARIES - Link these to use WebKitPart
 #  WEBKITPART_DEFINITIONS - Compiler switches required for using WebKitPart
+#
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
-
 
 if ( WEBKITPART_INCLUDE_DIR AND WEBKITPART_LIBRARIES )
    # in cache already
@@ -17,7 +16,7 @@ endif ( WEBKITPART_INCLUDE_DIR AND WEBKITPART_LIBRARIES )
 
 # Little trick I found in FindKDE4Interal... If we're building WebKitPart, set the variables to point to the build directory.
 if(webkitpart_SOURCE_DIR)
-    set(WEBKITPART_LIBRARIES webkitkpart)
+    set(WEBKITPART_LIBRARIES kwebkitpart)
     set(WEBKITPART_INCLUDE_DIR ${CMAKE_SOURCE_DIR})
 endif(webkitpart_SOURCE_DIR)
 
@@ -25,18 +24,15 @@ endif(webkitpart_SOURCE_DIR)
 # in the FIND_PATH() and FIND_LIBRARY() calls
 if( NOT WIN32 )
   find_package(PkgConfig)
-
   pkg_check_modules(PC_WEBKITPART kwebkitpart)
-
   set(PCRE_DEFINITIONS ${PC_WEBKITPART_CFLAGS_OTHER})
 endif( NOT WIN32 )
 
-FIND_PATH(WEBKITPART_INCLUDE_DIR NAMES webkitpart.h
+FIND_PATH(WEBKITPART_INCLUDE_DIR NAMES kwebkitpart.h
   PATHS
   ${PC_WEBKITPART_INCLUDEDIR} 
   ${PC_WEBKITPART_INCLUDE_DIRS}
   ${KDE4_INCLUDE_INSTALL_DIR}
-  PATH_SUFFIXES webkitkde
 )
 
 FIND_LIBRARY(WEBKITPART_LIBRARIES NAMES kwebkit
@@ -51,4 +47,3 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(WebKitPart DEFAULT_MSG WEBKITPART_INCLUDE_DIR 
 
 # show the WEBKITPART_INCLUDE_DIR and WEBKITPART_LIBRARIES variables only in the advanced view
 MARK_AS_ADVANCED(WEBKITPART_INCLUDE_DIR WEBKITPART_LIBRARIES)
-
