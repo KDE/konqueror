25019ed5da188fdc47886c8fa574c090f86bee1c
Benjamin Reed <ranger@befunk.com>
2010-03-05 17:54:58 +0000

    test for sys/proc_info.h and sys/proc.h
    proc_info.h was introduced in OSX 10.5
    10.4 (and perhaps earlier, although untested)
    contained the necessary defines in sys/proc.h.
    
    svn path=/branches/KDE/4.4/kdebase/apps/; revision=1099510

---
+++ konsole/src/config-konsole.h.cmake
@@ -0,0 +1,14 @@
+/* Defines whether /proc/`pid`/cwd exists */
+#cmakedefine HAVE_PROC_CWD 1
+
+/* Define if you have the XKB extension */
+#cmakedefine HAVE_XKB 1
+
+/* Defined if your system has XRender support */
+#cmakedefine HAVE_XRENDER 1
+
+/* Defined if you have sys/proc.h */
+#cmakedefine HAVE_SYS_PROC_H 1
+
+/* Defined if you have sys/proc_info.h */
+#cmakedefine HAVE_SYS_PROC_INFO_H 1
