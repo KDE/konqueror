01688963f85cad0d92465bda3ebe96218c5992aa
Markus Meik Slopianka <markus.s@kdemail.net>
2010-07-25 23:54:59 +0000

    Modified (some) 'About' windows to carry legally enforceable Copyright messages. Also looks better than '(c)'. ;-) Also fixed a few typos where I noticed them.
    svn path=/trunk/KDE/kdebase/apps/; revision=1154600

--- kfind/main.cpp
+++ kfind/main.cpp
@@ -1,19 +1,19 @@
 /*******************************************************************
 * main.cpp
-* 
+*
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation; either version 2 of 
+* published by the Free Software Foundation; either version 2 of
 * the License, or (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* 
+*
 ******************************************************************/
 
 #include <QtCore/QDir>
@@ -33,7 +33,7 @@ int main( int argc, char ** argv )
 {
   KAboutData aboutData( "kfind", "kfindpart", ki18n("KFind"),
       KFIND_VERSION, ki18n(description), KAboutData::License_GPL,
-      ki18n("(c) 1998-2003, The KDE Developers"));
+      ki18n("Copyright © 1998–2011 KFind authors"));
 
   aboutData.addAuthor(ki18n("Eric Coquelle"), ki18n("Current Maintainer"), "coquelle@caramail.com");
   aboutData.addAuthor(ki18n("Mark W. Webb"), ki18n("Developer"), "markwebb@adelphia.net");
