d58684c44804744105bcedb257440868c95e1ff1
Stefano Crocco <stefano.crocco@alice.it>
2015-01-27 19:25:04 +0100

    Ported shellcmdplugin to KF5
    Small changes in CMakeLists.txt and include files
    
    REVIEW: 122280

--- konq-plugins/shellcmdplugin/kshellcmdplugin.cpp
+++ konq-plugins/shellcmdplugin/kshellcmdplugin.cpp
@@ -19,7 +19,6 @@
 #include "kshellcmdplugin.h"
 #include "kshellcmddialog.h"
 #include <kparts/part.h>
-#include <kicon.h>
 #include <kactioncollection.h>
 #include <kinputdialog.h>
 #include <klocale.h>
@@ -30,6 +29,12 @@
 #include <kauthorized.h>
 #include <kio/netaccess.h>
 #include <kparts/fileinfoextension.h>
+#include <KParts/ReadOnlyPart>
+#include <QAction>
+
+//KDELibs4Support
+#include <kurl.h>
+#include <kicon.h>
 
 KShellCmdPlugin::KShellCmdPlugin( QObject* parent, const QVariantList & )
     : KParts::Plugin( parent )
@@ -37,7 +42,7 @@ KShellCmdPlugin::KShellCmdPlugin( QObject* parent, const QVariantList & )
     if (!KAuthorized::authorizeKAction("shell_access"))
        return;
 
-    KAction *action = actionCollection()->addAction("executeshellcommand");
+    QAction *action = actionCollection()->addAction("executeshellcommand");
     action->setIcon(KIcon("system-run"));
     action->setText(i18n( "&Execute Shell Command..." ));
     connect(action, SIGNAL(triggered(bool)), SLOT(slotExecuteShellCommand()));
@@ -93,7 +98,6 @@ void KShellCmdPlugin::slotExecuteShellCommand()
 }
 
 K_PLUGIN_FACTORY(KonqShellCmdPluginFactory, registerPlugin<KShellCmdPlugin>();)
-K_EXPORT_PLUGIN(KonqShellCmdPluginFactory("kshellcmdplugin"))
 
 #include "kshellcmdplugin.moc"
 
