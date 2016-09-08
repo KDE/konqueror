e6c1cc3b93b67e7e9265e811e13ba283dfdc7f0f
Sune Vuorela <sune@vuorela.dk>
2016-09-08 16:57:24 +0200

    Remove more error handling code

--- src/kwebkitpart.cpp
+++ src/kwebkitpart.cpp
@@ -81,17 +81,6 @@
 
 K_GLOBAL_STATIC_WITH_ARGS(QUrl, globalBlankUrl, ("about:blank"))
 
-static inline int convertStr2Int(const QString& value)
-{
-   bool ok;
-   const int tempValue = value.toInt(&ok);
-
-   if (ok)
-     return tempValue;
-
-   return 0;
-}
-
 KWebKitPart::KWebKitPart(QWidget *parentWidget, QObject *parent,
                          const QByteArray& cachedHistory, const QStringList& /*args*/)
             :KParts::ReadOnlyPart(parent),
@@ -374,37 +363,6 @@ bool KWebKitPart::openUrl(const QUrl &_u)
     // Pointer to the page object...
     WebPage* p = page();
     Q_ASSERT(p);
-
-//TODO: Porting
-//    // Handle error conditions...
-//    if (u.protocol().compare(QL1S("error"), Qt::CaseInsensitive) == 0 && u.hasSubUrl()) {
-//        /**
-//         * The format of the error url is that two variables are passed in the query:
-//         * error = int kio error code, errText = QString error text from kio
-//         * and the URL where the error happened is passed as a sub URL.
-//         */
-//        KUrl::List urls = KUrl::split(u);
-
-//        if ( urls.count() > 1 ) {
-//            KUrl mainURL = urls.first();
-//            int error = convertStr2Int(mainURL.queryItem("error"));
-
-//            // error=0 isn't a valid error code, so 0 means it's missing from the URL
-//            if ( error == 0 )
-//                error = KIO::ERR_UNKNOWN;
-
-//            const QString errorText = mainURL.queryItem( "errText" );
-//            urls.pop_front();
-//            KUrl reqUrl = KUrl::join( urls );
-//            emit m_browserExtension->setLocationBarUrl(reqUrl.prettyUrl());
-//            if (p) {
-//                m_webView->setHtml(p->errorPage(error, errorText, reqUrl));
-//                return true;
-//            }
-//        }
-
-//        return false;
-//    }
 
     KParts::BrowserArguments bargs (m_browserExtension->browserArguments());
     KParts::OpenUrlArguments args (arguments());
