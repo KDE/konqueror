0d847949c93999c7e1b9aede9d17beabd8985ba5
David Faure <faure@kde.org>
2016-09-10 06:32:19 +0200

    Remove outdated (khtml-specific, not compiling) nsplugins subdir.

--- nsplugins/Messages.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-#	$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$EXTRACTRC `find . -name "*.rc"` >> rc.cpp || exit 12
-$XGETTEXT *.cpp viewer/*.cpp -o $podir/nsplugin.pot
-rm -f rc.cpp
