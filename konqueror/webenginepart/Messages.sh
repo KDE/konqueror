76d4a86f9f9b0e3873e2fe95faa1b5011a581a40
Pino Toscano <pino@kde.org>
2016-10-29 09:06:19 +0200

    i18n: do not extract messages from 'tests' subdirs

--- konqueror/webenginepart/Messages.sh
+++ konqueror/webenginepart/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name '*.rc' -or -name '*.ui'` >> rc.cpp || exit 11
-$XGETTEXT `find . -name '*.cpp'` -o $podir/webenginepart.pot
+$XGETTEXT `find . -name '*.cpp' | grep -v '/tests/'` -o $podir/webenginepart.pot
 rm -f rc.cpp
