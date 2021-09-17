bf4ab616309d4fd99456a8329a0a9d021d88a8c8
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-17 17:00:23 +0200

    Convert KCachegrind license headers to SPDX

--- plugins/fsview/fsview_part.cpp
+++ plugins/fsview/fsview_part.cpp
@@ -3,19 +3,7 @@
     Some file management code taken from the Dolphin file manager (C) 2006-2009,
     by Peter Penz <peter.penz19@mail.com>
 
-    KCachegrind is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public
-    License as published by the Free Software Foundation, version 2.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; see the file COPYING.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: GPL-2.0-only
 */
 
 /*
