6e57dd6a3bacbdda5c36d52bbc78464f997bdf53
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-19 06:38:44 +0200

    Convert remaining license headers & copyright statements to SPDX

--- plugins/webarchiver/plugin_webarchiver.h
+++ plugins/webarchiver/plugin_webarchiver.h
@@ -1,19 +1,7 @@
 /* This file is part of Webarchiver
     SPDX-FileCopyrightText: 2001 Andreas Schlapbach <schlpbch@iam.unibe.ch>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this program; see the file COPYING.  If not, see
-    <http://www.gnu.org/licenses/>.
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #ifndef PLUGIN_WEBARCHIVER_H
