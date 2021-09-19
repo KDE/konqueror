67034175f06d79a297b71a5495e0bd1f8bf1b287
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-19 06:54:45 +0200

    Add missing SPDX-FileCopyrightText tags

--- plugins/kimgalleryplugin/imgallerydialog.cpp
+++ plugins/kimgalleryplugin/imgallerydialog.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
 
 SPDX-FileCopyrightText: 2001, 2003 Lukas Tinkl <lukas@kde.org>
-Andreas Schlapbach <schlpbch@iam.unibe.ch>
+SPDX-FileCopyrightText: Andreas Schlapbach <schlpbch@iam.unibe.ch>
 
 SPDX-License-Identifier: LGPL-2.0-only
 */
