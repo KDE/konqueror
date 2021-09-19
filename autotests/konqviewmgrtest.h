152727a2ecfe4bd810f99fd2b34a00199aea6e42
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-09-19 06:44:31 +0200

    Clean up leftovers statements in license headers
    That information is already stated using the SPDX-License-Identifier statement

--- autotests/konqviewmgrtest.h
+++ autotests/konqviewmgrtest.h
@@ -2,7 +2,6 @@
     SPDX-FileCopyrightText: 2006 David Faure <faure@kde.org>
 
     SPDX-License-Identifier: GPL-2.0-or-later
-    Boston, MA 02110-1301, USA.
 */
 
 #ifndef KONQVIEWMGRTEST_H
--- webenginepart/autotests/webenginepartcookiejar_test.h
+++ webenginepart/autotests/webenginepartcookiejar_test.h
@@ -3,16 +3,6 @@
     SPDX-FileCopyrightText: 2018 Stefano Crocco <stefano.crocco@alice.it>
 
     SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
 */
 
 #ifndef TESTWEBENGINEPARTCOOKIEJAR_H
