4f38fdb02f41b289fd367059a63f4e1ba41179e9
Stefano Crocco <stefano.crocco@alice.it>
2023-08-21 10:14:25 +0000

    Add missing #include

--- webenginepart/src/qtwebengine6compat.h
+++ webenginepart/src/qtwebengine6compat.h
@@ -1,6 +1,8 @@
 #ifndef QTWEBENGINE6COMPAT_H
 #define QTWEBENGINE6COMPAT_H
 
+#include <QtGlobal>
+
 //TODO KF6: when dropping support for Qt5, remove this file and include the files in the #else branch
 #if QT_VERSION_MAJOR < 6
 #include <QWebEngineDownloadItem>
