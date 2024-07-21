b38f40cab94ab3302d93dae2891e9625a46af85b
Stefano Crocco <stefano.crocco@alice.it>
2024-07-21 08:40:18 +0000

    Use KParts::PartLoader::partCapabilities instead of reading ServiceTypes when possible

--- src/konqutils.cpp
+++ src/konqutils.cpp
@@ -8,10 +8,28 @@
 
 #include "konqutils.h"
 
+#include <kparts_version.h>
+#include <KParts/PartLoader>
+
 #include <QJsonObject>
 
 QStringList Konq::serviceTypes(const KPluginMetaData& md)
 {
     //TODO KF6: ensure that this entry will still exist
+#if KPARTS_VERSION >= QT_VERSION_CHECK(6,4,0)
+    KParts::PartCapabilities capabilities = KParts::PartLoader::partCapabilities(md);
+    QStringList serviceTypes;
+    if (capabilities & KParts::PartCapability::ReadOnly) {
+        serviceTypes << QLatin1String("KParts/ReadOnlyPart");
+    }
+    if (capabilities & KParts::PartCapability::ReadWrite) {
+        serviceTypes <<  QStringLiteral("KParts/ReadWritePart");
+    }
+    if (capabilities & KParts::PartCapability::BrowserView) {
+        serviceTypes << QStringLiteral("Browser/View");
+    }
+    return serviceTypes;
+#else
     return md.rawData().value(QLatin1String("KPlugin")).toObject().value(QLatin1String("ServiceTypes")).toVariant().toStringList();
+#endif
 }
