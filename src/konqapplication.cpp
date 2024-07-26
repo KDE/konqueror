KDE/konqueror

commit deb57513797f953bd8d3835bed3abae80e2f7e2f

Improve behavior when launching with --load-session and an invalid session

src/konqapplication.cpp
@@ -38,6 +38,7 @@
 #include <KStartupInfo>
 #include <KWindowSystem>
 #include <kwindowsystem_version.h>
+#include <KMessageBox>
 
 #include <iostream>
 
@@ -244,7 +245,19 @@ int KonquerorApplication::performStart(const QString& workingDirectory, bool fir
         listSessions();
         return 0;
     } else if (m_parser.isSet("open-session")) {
-        return openSession(m_parser.value("open-session"));
+        //If the given session can't be opened for any reason, inform the user
+        QString sessionName = m_parser.value("open-session");
+        int result = openSession(sessionName);
+        if (result != 0) {
+            KMessageBox::sorry(nullptr, i18nc("The session asked by the user doesn't exist or can't be opened",
+                                              "Session %1 couldn't be opened", sessionName));
+        }
+        //If there was an error opening the session and this is the first instance, don't return immediately
+        //as this would lead to the application being run but with no windows open. Instead, open an empty
+        //window
+        if (result != 0 && firstInstance) {
+            return result;
+        }
     }
 
     //We check for the --preload switch before attempting recovering session because we shouldn't

commit a43170756e2295c67cc5b023ca452dd4e55de3f8

Fix logic error in konqueror --open-session
Don't open extra window when openSession succeeds.

Fixes: deb575137 ("Improve behavior when launching with --load-session and an invalid session")

src/konqapplication.cpp
@@ -344,10 +344,11 @@ int KonquerorApplication::performStart(const QString& workingDirectory, bool fir
             KMessageBox::error(nullptr, i18nc("The session asked by the user doesn't exist or can't be opened",
                                               "Session %1 couldn't be opened", sessionName));
         }
-        //If there was an error opening the session and this is the first instance, don't return immediately
-        //as this would lead to the application being run but with no windows open. Instead, open an empty
+        //Return except when this is the first instance and there was an error loading the session,
+        //since in this case, returning would lead to the application being run with no windows. Instead
+        //go on with the function so an empty window will be created
         //window
-        if (result != 0 && firstInstance) {
+        if (result == 0 || !firstInstance) {
             return result;
         }
     }