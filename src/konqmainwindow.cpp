c6369d59d3266ad699383973c277419ac5381b58
Stefano Crocco <stefano.crocco@alice.it>
2024-07-28 12:19:45 +0200

    Don't autosave settings for preloaded windows

--- src/konqmainwindow.cpp
+++ src/konqmainwindow.cpp
@@ -4491,6 +4491,13 @@ void KonqMainWindow::readProperties(const KConfigGroup &configGroup)
 
 void KonqMainWindow::applyMainWindowSettings(const KConfigGroup &config)
 {
+    //Don't save settings for preloaded windows to avoid override changes in size and position made by the user
+    //(since preloaded windows are invisible the user won't be able to interact with them, which means that they'll
+    //have old or default settings)
+    if (isPreloaded()) {
+        return;
+    }
+
     KParts::MainWindow::applyMainWindowSettings(config);
     if (m_currentView) {
         /// @Note status bar isn't direct child to main window
